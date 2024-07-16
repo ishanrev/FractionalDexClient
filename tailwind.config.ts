5149c727d9c976745943276e1f01e169c47e2e60
ishanrev <ishanrev123@gmail.com>
2024-07-16 14:53:26 -0400

    added next app with base configurations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,28 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        background: {
+          DEFAULT: '#1B1F3B', // Midnight Blue
+          coal: '#121212',
+
+        },
+        button: {
+          primary: '#FFBF00', // Amber
+          secondary: '#008080', // Teal
+          base: '#1f2937 '
+        },
+        text: {
+          DEFAULT: '#B0B0B0', // Medium Gray
+        },
+        border: {
+          DEFAULT: '#E0E0E0', // Light Gray
+        },
+        accent: {
+          cyan: '#00FFFF', // Cyan
+          limeGreen: '#32CD32', // Lime Green
+        },
+      },
     },
   },
   plugins: [],
